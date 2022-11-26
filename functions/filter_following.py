e64f45d9faead729baa7a1cf07407a59a3dce7af
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2022-11-26 10:10:44 +0000

    separate functions

---
+++ functions/filter_following.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+import pandas as pd
+
+followers_file = "data/RCONfollowers.csv"
+nurse_description_string = "nurse|nursing"
+today = datetime.date.today()
+current_year = today.year
+
+def filter(description_string = nurse_description_string, df = None):
+    #filter description
+    df["description"] = df["description"].fillna(value="None")
+    df = df[df["description"].str.contains(description_string)]
+
+    #filter by rate of tweeting
+    #get years since creation of account and total number of tweets
+    #to get tweet per year
+    df["year_created"] = df["created_at"]
+    df["year_created"] = df.year_created.str[-4:]
+    df["year_created"] = df["year_created"].astype(int)
+    df = df[df["year_created"]<2019]
+    df["years_online"] = int(current_year) - df["year_created"]
+    df["tweets_per_year"] = df["statuses_count"] / df["years_online"]
+
+    return df
+
+
+followersdf = pd.read_csv(followers_file, index_col=0)
+followersdf = filter(nurse_description_string,df=followersdf)
+followersdf.to_csv(followers_file)
+
--- functions/get_following.py
+++ functions/get_following.py
@@ -5,14 +5,7 @@ import pandas as pd
 from authpy import authpy
 import datetime
 
-today = datetime.date.today()
-
-current_year = today.year
-
 followers_file = "data/RCONfollowers.csv"
-nurse_description_string = "nurse|nursing"
-
-
 api = authpy('credentials.json')
 
 try:
@@ -21,37 +14,22 @@ try:
 except:
     print('Failed authentication')
 
-def filter(description_string = nurse_description_string, df = None):
-    #filter description
-    df["description"] = df["description"].fillna(value="None")
-    df = df[df["description"].str.contains(description_string)]
 
-    #filter by rate of tweeting
-    #get years since creation of account and total number of tweets
-    #to get tweet per year
-    df['year_created'] = df["created_at"].str[-4:]
-    df['year_created'] = df['year_created'].astype(int)
-    df['years_online'] = int(current_year) - df["year_created"]
-    df['tweets_per_year'] = df['statuses_count'] / df['years_online']
-
-    return df
-
-def get_union_followers(user_id = "54506896", pagination = None):
+def get_union_followers(pagination = None):
     if pagination:
-        onehundredfollowers, tokens = api.get_followers(user_id=user_id, cursor=pagination, count=100, skip_status=True)
+        onehundredfollowers, tokens = api.get_followers(user_id=54506896, cursor=pagination, count=100, skip_status=True)
     else:
-        onehundredfollowers, tokens = api.get_followers(user_id=user_id, cursor=-1, count=100, skip_status=True)
+        onehundredfollowers, tokens = api.get_followers(user_id=54506896, cursor="-1", count=100, skip_status=True)
 
     followers_data = [r._json for r in onehundredfollowers]
+
     try:
         followersdf = pd.read_csv(followers_file, index_col=0)
     except pd.errors.EmptyDataError:
-        # followersdf = pd.DataFrame()
-        followersdf = pd.DataFrame({'id':[], 'id_str':[], 'name':[], 'screen_name':[], 'description':[], 'statuses_count':[], 'tweets_per_year':[], 'location':[]})
+        followersdf = pd.DataFrame({'id':[], 'id_str':[], 'name':[], 'screen_name':[], 'description':[],  'years_online':[], 'statuses_count':[], 'tweets_per_year':[], 'location':[]})
 
-    new_followers = pd.DataFrame(followers_data)
-    followersdf = pd.concat([followersdf, new_followers], ignore_index=True)
-    followersdf = filter(description_string=nurse_description_string, df=followersdf)
+    new_followers_df = pd.DataFrame(followers_data)
+    followersdf = pd.concat([followersdf, new_followers_df], ignore_index=True)
     followersdf.to_csv(followers_file)
 
     #get next token
@@ -60,10 +38,10 @@ def get_union_followers(user_id = "54506896", pagination = None):
 
 call_count = 0
 
-next_token = get_union_followers(pagination=1750455016348240442)
+next_token = get_union_followers()
 call_count += 1
 
-while next_token:
+while call_count<2:
     next_token = get_union_followers(next_token)
     call_count += 1
 
