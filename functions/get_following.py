9adcb47ae6af711833c0ad6ae3c07c73c89ea232
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2022-11-28 22:06:17 +0000

    new token RCON

--- functions/get_following.py
+++ functions/get_following.py
@@ -47,7 +47,7 @@ def get_union_followers(pagination = None):
 
 call_count = 0
 
-next_token = get_union_followers(1689129492845940907)
+next_token = get_union_followers(1644235140132012171)
 call_count += 1
 
 while next_token:
