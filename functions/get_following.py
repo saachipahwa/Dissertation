0a1a32516425de0e23cd86b0bac32ff8c713574f
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2022-11-29 12:11:35 +0000

    get bma followers again

--- functions/get_following.py
+++ functions/get_following.py
@@ -5,12 +5,12 @@ import pandas as pd
 from authpy import authpy
 import datetime
 
-followers_file = "data/moreRCONfollowers.csv"
+followers_file = "data/BMAfollowers.csv"
 api = authpy('credentials.json')
 RCON_ID = 54506896
 NEU_ID = 884369177368199168
 BMA_ID = 14243046 #BMA call 121: 1678465743253206942
-UNION_ID = RCON_ID
+UNION_ID = BMA_ID
 
 def store_log(message):
     print(message)
@@ -47,7 +47,7 @@ def get_union_followers(pagination = None):
 
 call_count = 0
 
-next_token = get_union_followers(1515197255314193782)
+next_token = get_union_followers(14243046)
 call_count += 1
 
 while next_token:
