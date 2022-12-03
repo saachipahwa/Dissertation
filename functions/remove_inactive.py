e3215c33fde0192f8346b47ae65e4712da671834
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2022-12-03 15:14:51 +0000

    Revert to 56e05fced

--- functions/remove_inactive.py
+++ functions/remove_inactive.py
@@ -12,12 +12,12 @@ for filename in os.listdir(directory):
         lastrow = df.iloc[-1]
         date = lastrow["created_at"]
         year = date[0:4]
-        month = date[5:7]
-        day = date[8:10]
+        month = date[5:6]
+        day = date[8:9]
         print("year: " + year)
         print("month: " + month)
         print("day: " + day)
-        # if int(year) < 2022:
-        #     os.remove(f)
-        # if int(year) == 2022 and int(month)<8:
-        #     os.remove(f)
+        if int(year) < 2022:
+            os.remove(f)
+        if int(year) == 2022 and int(month)<8:
+            os.remove(f)
