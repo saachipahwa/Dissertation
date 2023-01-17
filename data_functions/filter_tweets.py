7a631d52a0dc0348f2e43e2f2fa97f94237b6fb6
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-17 15:08:19 +0000

    removing unused filter functions (including language one)

--- data_functions/filter_tweets.py
+++ data_functions/filter_tweets.py
@@ -21,10 +21,7 @@ from nltk.corpus import treebank
 #Text preprocessing
 #REFERENCE: https://www.analyticsvidhya.com/blog/2021/06/text-preprocessing-in-nlp-with-python-codes/
 
-#Helper function removing punctuations like . , ! $( ) * % @
-# def remove_punctuation_string(text):
-#     punctuationfree="".join([i for i in text if i not in string.punctuation])
-#     return punctuationfree
+
 
 detector_factory.seed = 30
 
@@ -39,9 +36,6 @@ def remove_hashtags_mentions(text):
     clean_tweet = re.sub("#[A-Za-z0-9_]+","", clean_tweet)
     return clean_tweet
 
-# # Removing numbers
-# def remove_numbers(text):
-#     return ''.join((x for x in text if not x.isdigit()))
 
 def remove_nonalphabet(text):
     regex = re.compile('[^a-zA-Z\s\']')
@@ -65,18 +59,6 @@ def too_short(text):
         return True
     return False
 
-def remove_less_5_words(df):
-    df['too_short'] = df['clean_text'].apply(lambda x:str(too_short(x)))
-    df = df[df['too_short'].str.contains('False') == True]
-    df = df.drop('too_short', axis=1)
-    return df
-
-# Stemming
-# porter_stemmer = PorterStemmer()
-# def stemming(text):
-#     stem_text = [porter_stemmer.stem(word) for word in text]
-#     return stem_text
-
 # Lemmatization
 wordnet_lemmatizer = WordNetLemmatizer()
 def lemmatizer(text):
@@ -84,34 +66,11 @@ def lemmatizer(text):
     lemm_text = [wordnet_lemmatizer.lemmatize(word) for word in tokens]
     return(' '.join(lemm_text))
 
-#Remove words not in english
-words = set(nltk.corpus.words.words())
-def nonenglish(text):
-    try:
-        lang = detect(str(text))
-        if lang == "en":
-            return str(False)
-        else:
-            print("not english", text, lang)
-            print(text, detect_langs(text))
-        return str(True)
-    except:
-        print(text, "throws an error")
-        return None
-
-# def return_non_english(text):
-#     return " ".join(w for w in nltk.wordpunct_tokenize(text) \
-#              if w.lower() in words or not w.isalpha())
-
-# print(nonenglish(" me too freddie xx"))
-# print(nonenglish(" i see my good friend"))
-
 def delete_whitespace_tweets(df):
     df['whitespace'] = df['clean_text'].apply(lambda x:str(str(x).isspace()))
     df = df[df['whitespace'].str.contains('False') == True]
     df = df.drop('whitespace', axis=1)
     return df
-
 
 def text_preprocessing(directory = "journalisttweets"):
     for filename in os.listdir(directory):
@@ -138,16 +97,9 @@ def text_preprocessing(directory = "journalisttweets"):
             #remove non alphabet chars
             df['clean_text'] = df['clean_text'].apply(lambda x:remove_nonalphabet(x))
 
-            # df = remove_less_5_words(df)
 
             #lower case
             df['clean_text'] = df['clean_text'].apply(lambda x: x.lower())
-
-            #remove non english tweets
-            df['non_english'] = df['clean_text']
-            df['non_english'] = df['non_english'].apply(lambda x:nonenglish(x))
-
-            # df = df[df['non_english'].str.contains('False') == True]
 
             #lemmatizing
             df['clean_text'] = df['clean_text'].apply(lambda x:lemmatizer(str(x)))
