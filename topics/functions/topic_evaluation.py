ac3779bfcffa4464e8c1f0421e15a25235968c4d
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-21 16:07:37 +0000

    topic evaluation

---
+++ topics/functions/topic_evaluation.py
@@ -0,0 +1,8 @@
+from bertopic import BERTopic
+from octis.evaluation_metrics.diversity_metrics import TopicDiversity
+
+model_5 = BERTopic.load("topics/models/nursetweets_5_model")
+print("starting evaluation")
+metric = TopicDiversity(topk=10)
+print("doing evaluation for", "5")
+print(metric.score(model_5))
