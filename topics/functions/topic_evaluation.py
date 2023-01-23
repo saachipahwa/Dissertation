3460acd41be6967ebb2049d162d44ae502af44fa
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-23 16:04:17 +0000

    topic evaluation test code

--- topics/functions/topic_evaluation.py
+++ topics/functions/topic_evaluation.py
@@ -3,26 +3,27 @@ from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 import pandas as pd
 
 #set up evaluation spreadsheet
-data= {'nr_topics': [5, 10, 15, 20], 'topic_diversity': [None, None, None, None]}
-evaluation_df = pd.DataFrame(data)
-evaluation_df.set_index('nr_topics')
-evaluation_df.to_csv("topics/topic_evaluation.csv")
-print("set up evaluation csv")
+# data= {'nr_topics': [5, 10, 15, 20], 'topic_diversity': [None, None, None, None]}
+# evaluation_df = pd.DataFrame(data)
+# evaluation_df.set_index('nr_topics')
+# evaluation_df.to_csv("topics/topic_evaluation.csv")
+# print("set up evaluation csv")
 
 #evaluation
 model_5 = BERTopic.load("topics/models/nursetweets_5_model")
-metric = TopicDiversity(topk=10)
-score = metric.score(model_5)
-print(score)
+metric = TopicDiversity(topk=5)
+score = metric.score({"topics":list(model_5.get_topics().values())})
+print("model 5 score", score)
 
 model_10 = BERTopic.load("topics/models/nursetweets_10_model")
 print("doing evaluation for", "10")
-evaluation_df.at[10,'topic_diversity'] = metric.score(model_10)
+print("model 10 score", metric.score({"topics":list(model_10.get_topics().values())}))
 
 model_15 = BERTopic.load("topics/models/nursetweets_15_model")
 print("doing evaluation for", "15")
-evaluation_df.at[15,'topic_diversity'] = metric.score(model_15)
+print("model 15 score",  metric.score({"topics":list(model_15.get_topics().values())}))
 
 model_20 =  BERTopic.load("topics/models/nursetweets_20_model")
 print("doing evaluation for", "20")
-evaluation_df.at[20,'topic_diversity'] = metric.score(model_20)
+print("model 20 score", metric.score({"topics":list(model_20.get_topics().values())}))
+# evaluation_df.at[20,'topic_diversity'] = metric.score(model_20)
