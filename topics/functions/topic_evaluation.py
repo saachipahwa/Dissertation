ef616390c538a42e7fe1fd6eb11dfc97d5e87335
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-23 17:49:33 +0000

    topic diversity improved

--- topics/functions/topic_evaluation.py
+++ topics/functions/topic_evaluation.py
@@ -3,8 +3,7 @@ from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 import pandas as pd
 
 #set up evaluation spreadsheet
-data= {'nr_topics': [5, 10, 15, 20], 'topic_diversity': [None, None, None, None]}
-evaluation_df = pd.DataFrame(data)
+evaluation_df = pd.DataFrame(columns=['nr_topics', 'topic_diversity'])
 evaluation_df.set_index('nr_topics')
 evaluation_df.to_csv("topics/topic_evaluation.csv")
 print("set up evaluation csv")
@@ -23,30 +22,37 @@ def get_words_from_model(model):
         list_of_word_lists.append(get_words_from_topic(topic))
     return list_of_word_lists
 
-#evaluation
+def print_topic_words(model):
+    for i in list(model.get_topics().values):
+        print('topic {}:'.format(i), model.get_topic(i))
+
+#topic diversity evaluation
 metric = TopicDiversity(topk=10)
 
 model_5 = BERTopic.load("topics/models/nursetweets_5_model")
 score_5 = metric.score({"topics":get_words_from_model(model_5)})
 print("model 5 score", score_5)
-evaluation_df.at[5,'topic_diversity'] = score_5
-
+evaluation_df.loc[len(evaluation_df)] = [5, score_5]
 model_10 = BERTopic.load("topics/models/nursetweets_10_model")
 print("doing evaluation for", "10")
 score_10 = metric.score({"topics":get_words_from_model(model_10)})
 print("model 10 score", score_10)
-evaluation_df.at[10,'topic_diversity'] = score_10
+evaluation_df.loc[len(evaluation_df)] = [10, score_10]
 
 model_15 = BERTopic.load("topics/models/nursetweets_15_model")
 print("doing evaluation for", "15")
 score_15 = metric.score({"topics":get_words_from_model(model_15)})
 print("model 15 score", score_15)
-evaluation_df.at[15,'topic_diversity'] = score_15
+evaluation_df.loc[len(evaluation_df)] = [15, score_15]
 
 model_20 =  BERTopic.load("topics/models/nursetweets_20_model")
 print("doing evaluation for", "20")
 score_20 = metric.score({"topics":get_words_from_model(model_20)})
 print("model 20 score", score_20)
-evaluation_df.at[20,'topic_diversity'] = score_20
+evaluation_df.loc[len(evaluation_df)] = [20, score_20]
+
+#KL_model
+
+
 
 evaluation_df.to_csv("topics/topic_evaluation.csv")
