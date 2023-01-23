cea1d0a256c7c3a7208ab6085238897f7848830f
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-23 16:14:47 +0000

    more test code for topic diversity

--- topics/functions/topic_evaluation.py
+++ topics/functions/topic_evaluation.py
@@ -9,21 +9,36 @@ import pandas as pd
 # evaluation_df.to_csv("topics/topic_evaluation.csv")
 # print("set up evaluation csv")
 
+def get_words_from_topic(topic):
+    #get words without probabilities
+    words = []
+    for x,y in topic:
+        words.append(x)
+    return words
+
+def get_words_from_model(model):
+    topics_list = model_5.get_topics().values()
+    list_of_word_lists = []
+    for topic in topics_list:
+        list_of_word_lists.append(get_words_from_topic(topic))
+    return list_of_word_lists
+
 #evaluation
+metric = TopicDiversity(topk=10)
+
 model_5 = BERTopic.load("topics/models/nursetweets_5_model")
-metric = TopicDiversity(topk=5)
-score = metric.score({"topics":list(model_5.get_topics().values())})
+score = metric.score({"topics":get_words_from_model(model_5)})
 print("model 5 score", score)
 
 model_10 = BERTopic.load("topics/models/nursetweets_10_model")
 print("doing evaluation for", "10")
-print("model 10 score", metric.score({"topics":list(model_10.get_topics().values())}))
+print("model 10 score", metric.score({"topics":get_words_from_model(model_10)}))
 
 model_15 = BERTopic.load("topics/models/nursetweets_15_model")
 print("doing evaluation for", "15")
-print("model 15 score",  metric.score({"topics":list(model_15.get_topics().values())}))
+print("model 15 score", metric.score({"topics":get_words_from_model(model_15)}))
 
 model_20 =  BERTopic.load("topics/models/nursetweets_20_model")
 print("doing evaluation for", "20")
-print("model 20 score", metric.score({"topics":list(model_20.get_topics().values())}))
+print("model 20 score", metric.score({"topics":get_words_from_model(model_20)}))
 # evaluation_df.at[20,'topic_diversity'] = metric.score(model_20)
