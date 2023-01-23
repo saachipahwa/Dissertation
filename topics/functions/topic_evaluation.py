652e7d7a4af5bc0044c4598e284f3bc3a854f66d
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-23 16:20:06 +0000

    syntax

--- topics/functions/topic_evaluation.py
+++ topics/functions/topic_evaluation.py
@@ -27,26 +27,26 @@ def get_words_from_model(model):
 metric = TopicDiversity(topk=10)
 
 model_5 = BERTopic.load("topics/models/nursetweets_5_model")
-score = metric.score({"topics":get_words_from_model(model_5)})
-print("model 5 score", score)
-evaluation_df.at[5,'topic_diversity'] = metric.score(model_5)
+score_5 = metric.score({"topics":get_words_from_model(model_5)})
+print("model 5 score", score_5)
+evaluation_df.at[5,'topic_diversity'] = score_5
 
 model_10 = BERTopic.load("topics/models/nursetweets_10_model")
 print("doing evaluation for", "10")
-score = metric.score({"topics":get_words_from_model(model_10)})
-print("model 10 score", score)
-evaluation_df.at[10,'topic_diversity'] = metric.score(model_10)
+score_10 = metric.score({"topics":get_words_from_model(model_10)})
+print("model 10 score", score_10)
+evaluation_df.at[10,'topic_diversity'] = score_10
 
 model_15 = BERTopic.load("topics/models/nursetweets_15_model")
 print("doing evaluation for", "15")
-score = metric.score({"topics":get_words_from_model(model_15)})
-print("model 15 score", score)
-evaluation_df.at[15,'topic_diversity'] = metric.score(model_15)
+score_15 = metric.score({"topics":get_words_from_model(model_15)})
+print("model 15 score", score_15)
+evaluation_df.at[15,'topic_diversity'] = score_15
 
 model_20 =  BERTopic.load("topics/models/nursetweets_20_model")
 print("doing evaluation for", "20")
-score = metric.score({"topics":get_words_from_model(model_20)})
-print("model 20 score", score)
-evaluation_df.at[20,'topic_diversity'] = metric.score(model_20)
+score_20 = metric.score({"topics":get_words_from_model(model_20)})
+print("model 20 score", score_20)
+evaluation_df.at[20,'topic_diversity'] = score_20
 
 evaluation_df.to_csv("topics/topic_evaluation.csv")
