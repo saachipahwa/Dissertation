21357d02ced24f227419b005a886805eb2cbf923
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-24 12:25:39 +0000

    add kl metrics

--- topics/functions/topic_evaluation.py
+++ topics/functions/topic_evaluation.py
@@ -1,9 +1,10 @@
 from bertopic import BERTopic
+from octis.evaluation_metrics.topic_significance_metrics import KL_uniform, KL_vacuous, KL_background
 from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 import pandas as pd
 
 #set up evaluation spreadsheet
-evaluation_df = pd.DataFrame(columns=['nr_topics', 'topic_diversity'])
+evaluation_df = pd.DataFrame(columns=['nr_topics', 'topic_diversity', 'KL_uniform', 'KL_vacuous', 'KL_background'])
 evaluation_df.set_index('nr_topics')
 evaluation_df.to_csv("Dissertation/topics/topic_evaluation.csv")
 print("set up evaluation csv")
@@ -26,32 +27,46 @@ def print_topic_words(model):
     for i in list(model.get_topics().values):
         print('topic {}:'.format(i), model.get_topic(i))
 
-#topic diversity evaluation
-metric = TopicDiversity(topk=10)
 
+#load models and turn into dictionaries
 model_5 = BERTopic.load("nursetweets_5_model")
-score_5 = metric.score({"topics":get_words_from_model(model_5)})
-print("model 5 score", score_5)
-evaluation_df.loc[len(evaluation_df)] = [5, score_5]
-
+model_5_dict = {"topics":get_words_from_model(model_5)}
 model_10 = BERTopic.load("nursetweets_10_model")
-print("doing evaluation for", "10")
-score_10 = metric.score({"topics":get_words_from_model(model_10)})
-print("model 10 score", score_10)
-evaluation_df.loc[len(evaluation_df)] = [10, score_10]
-
+model_10_dict = {"topics":get_words_from_model(model_10)}
 model_15 = BERTopic.load("nursetweets_15_model")
-print("doing evaluation for", "15")
-score_15 = metric.score({"topics":get_words_from_model(model_15)})
-print("model 15 score", score_15)
-evaluation_df.loc[len(evaluation_df)] = [15, score_15]
-
+model_15_dict= {"topics":get_words_from_model(model_15)}
 model_20 =  BERTopic.load("nursetweets_20_model")
-print("doing evaluation for", "20")
-score_20 = metric.score({"topics":get_words_from_model(model_20)})
-print("model 20 score", score_20)
-evaluation_df.loc[len(evaluation_df)] = [20, score_20]
+model_20_dict = {"topics":get_words_from_model(model_20)}
 
-#KL
+#topic diversity evaluation
+TD_metric = TopicDiversity(topk=10)
+
+TD_score_5 = TD_metric.score(model_5_dict)
+print("model 5 score", TD_score_5)
+
+TD_score_10 = TD_metric.score(model_10_dict)
+print("model 10 score", TD_score_10)
+
+TD_score_15 = TD_metric.score(model_15_dict)
+print("model 15 score", TD_score_15)
+
+TD_score_20 = TD_metric.score(model_20_dict)
+print("model 20 score", TD_score_20)
+
+#KL metrics
+KLu_metric = KL_uniform()
+KLv_metric = KL_vacuous()
+
+KLb_metric = KL_background()
+
+KL_scores_5 = [KLu_metric.score(model_5_dict), KLv_metric.score(model_5_dict), KLb_metric.score(model_5_dict)]
+KL_scores_10 = [KLu_metric.score(model_10_dict), KLv_metric.score(model_10_dict), KLb_metric.score(model_10_dict)]
+KL_scores_15 = [KLu_metric.score(model_15_dict), KLv_metric.score(model_15_dict), KLb_metric.score(model_15_dict)]
+KL_scores_20 = [KLu_metric.score(model_20_dict), KLv_metric.score(model_20_dict), KLb_metric.score(model_20_dict)]
+
+evaluation_df.loc[len(evaluation_df)] = [5, TD_score_5, KL_scores_5[0], KL_scores_5[1], KL_scores_5[2]]
+evaluation_df.loc[len(evaluation_df)] = [10, TD_score_10, KL_scores_10[0], KL_scores_10[1], KL_scores_10[2]]
+evaluation_df.loc[len(evaluation_df)] = [15, TD_score_15, KL_scores_15[0], KL_scores_15[1], KL_scores_15[2]]
+evaluation_df.loc[len(evaluation_df)] = [20, TD_score_20, KL_scores_20[0], KL_scores_20[1], KL_scores_20[2]]
 
 evaluation_df.to_csv("Dissertation/topics/topic_evaluation.csv")
