39c79b8a2db533465695999b4633bd9150ac7455
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-01-24 12:07:00 +0000

    change nursemodels path

--- topics/functions/topic_evaluation.py
+++ topics/functions/topic_evaluation.py
@@ -29,27 +29,29 @@ def print_topic_words(model):
 #topic diversity evaluation
 metric = TopicDiversity(topk=10)
 
-model_5 = BERTopic.load("topics/models/nursetweets_5_model")
+model_5 = BERTopic.load("nursetweets_5_model")
 score_5 = metric.score({"topics":get_words_from_model(model_5)})
 print("model 5 score", score_5)
 evaluation_df.loc[len(evaluation_df)] = [5, score_5]
-model_10 = BERTopic.load("topics/models/nursetweets_10_model")
+
+model_10 = BERTopic.load("nursetweets_10_model")
 print("doing evaluation for", "10")
 score_10 = metric.score({"topics":get_words_from_model(model_10)})
 print("model 10 score", score_10)
 evaluation_df.loc[len(evaluation_df)] = [10, score_10]
 
-model_15 = BERTopic.load("topics/models/nursetweets_15_model")
+model_15 = BERTopic.load("nursetweets_15_model")
 print("doing evaluation for", "15")
 score_15 = metric.score({"topics":get_words_from_model(model_15)})
 print("model 15 score", score_15)
 evaluation_df.loc[len(evaluation_df)] = [15, score_15]
 
-model_20 =  BERTopic.load("topics/models/nursetweets_20_model")
+model_20 =  BERTopic.load("nursetweets_20_model")
 print("doing evaluation for", "20")
 score_20 = metric.score({"topics":get_words_from_model(model_20)})
 print("model 20 score", score_20)
 evaluation_df.loc[len(evaluation_df)] = [20, score_20]
 
+#KL
 
 evaluation_df.to_csv("topics/topic_evaluation.csv")
