fa30145444431dc2d14ff60952c752e4435c232b
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-02-01 12:25:51 +0000

    no topic nr

--- topics/functions/experiment.py
+++ topics/functions/experiment.py
@@ -18,14 +18,13 @@ def get_all_tweets(directory = None):
         df = pd.concat([df, user_df], ignore_index=True)
     return df
 
-def get_topics_from(directory_name = "nursetweets", nr_topics=None, embeddings=None):
+def get_topics_from(directory_name = "nursetweets", embeddings=None):
     #set up model parameters
     topic_model = BERTopic(language="english",
                            calculate_probabilities=False,
                            verbose=True,
                            low_memory=True,
                            n_gram_range=(1, 1),
-                           nr_topics=nr_topics
                            )
     print("set up topic model. about to fit model")
 
@@ -33,9 +32,9 @@ def get_topics_from(directory_name = "nursetweets", nr_topics=None, embeddings=N
     topics, probabilities = topic_model.fit_transform(tweet_text, embeddings)
     print("model has been fit")
 
-    open("{}_{}_model".format(directory_name, nr_topics), 'w+')
-    topic_model.save("{}_{}_model".format(directory_name, nr_topics))
-    print("model has been saved", nr_topics)
+    open("{}_test_model".format(directory_name,), 'w+')
+    topic_model.save("{}_test_model".format(directory_name))
+    print("model has been saved")
 
     freq = topic_model.get_topic_info()
     print(type(freq))
@@ -79,12 +78,12 @@ def pad_out_dict(dict):
 def get_docs():
     model5docs = pd.DataFrame.from_dict(pad_out_dict(model_5.get_representative_docs())).sort_index(axis=1)
     model5docs.to_csv('Dissertation/topics/nursetweets_5_docs.csv')
-    model10docs = pd.DataFrame.from_dict(pad_out_dict(model_10.get_representative_docs())).sort_index(axis=1)
-    model10docs.to_csv('Dissertation/topics/nursetweets_10_docs.csv')
-    model15docs = pd.DataFrame.from_dict(pad_out_dict(model_15.get_representative_docs())).sort_index(axis=1)
-    model15docs.to_csv('Dissertation/topics/nursetweets_15_docs.csv')
-    model20docs = pd.DataFrame.from_dict(pad_out_dict(model_20.get_representative_docs())).sort_index(axis=1)
-    model20docs.to_csv('Dissertation/topics/nursetweets_20_docs.csv')
+    # model10docs = pd.DataFrame.from_dict(pad_out_dict(model_10.get_representative_docs())).sort_index(axis=1)
+    # model10docs.to_csv('Dissertation/topics/nursetweets_10_docs.csv')
+    # model15docs = pd.DataFrame.from_dict(pad_out_dict(model_15.get_representative_docs())).sort_index(axis=1)
+    # model15docs.to_csv('Dissertation/topics/nursetweets_15_docs.csv')
+    # model20docs = pd.DataFrame.from_dict(pad_out_dict(model_20.get_representative_docs())).sort_index(axis=1)
+    # model20docs.to_csv('Dissertation/topics/nursetweets_20_docs.csv')
 
 #get tweet text
 tweet_text = get_tweets()
@@ -97,11 +96,11 @@ print("computing embeddings")
 #get topics
 print("getting topics ", "5")
 model_5 = get_topics_from(directory_name="nursetweets", nr_topics=5, embeddings=embeddings)
-print("getting topics ", "10")
-model_10 = get_topics_from(directory_name="nursetweets", nr_topics=10, embeddings=embeddings)
-print("getting topics ", "15")
-model_15 = get_topics_from(directory_name="nursetweets", nr_topics=15, embeddings=embeddings)
-print("getting topics for", "20")
-model_20 = get_topics_from(directory_name="nursetweets", nr_topics=20, embeddings=embeddings)
+# print("getting topics ", "10")
+# model_10 = get_topics_from(directory_name="nursetweets", nr_topics=10, embeddings=embeddings)
+# print("getting topics ", "15")
+# model_15 = get_topics_from(directory_name="nursetweets", nr_topics=15, embeddings=embeddings)
+# print("getting topics for", "20")
+# model_20 = get_topics_from(directory_name="nursetweets", nr_topics=20, embeddings=embeddings)
 
 get_docs()
