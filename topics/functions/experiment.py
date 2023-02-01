19b4f3f0eda57436f72b7f324480aa29d9c82443
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-02-01 12:24:34 +0000

    turn calculate prob off for experiment

--- topics/functions/experiment.py
+++ topics/functions/experiment.py
@@ -21,7 +21,7 @@ def get_all_tweets(directory = None):
 def get_topics_from(directory_name = "nursetweets", nr_topics=None, embeddings=None):
     #set up model parameters
     topic_model = BERTopic(language="english",
-                           calculate_probabilities=True,
+                           calculate_probabilities=False,
                            verbose=True,
                            low_memory=True,
                            n_gram_range=(1, 1),
