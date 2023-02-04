47492deaf97aad762d2029aac40198f3b985e143
Saachi Pahwa <saachi.pahwa.19@ucl.ac.uk>
2023-02-04 20:31:36 +0000

    read_csv

--- topics/classify/label_topics.py
+++ topics/classify/label_topics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.load_csv("Dissertation/topics/docs_topics.csv")
+df = pd.read_csv("Dissertation/topics/docs_topics.csv")
 
-df = df[df["Representative document"]==True]
+df = df[df["Representative_document"]==True]
 
 df.to_csv("representative_docs.csv")
