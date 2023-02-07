99e0d08f6f64da3dd6dd1aea0bb8862dd2bf62df
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-02-07 14:43:00 +0000

    set up to run again without advert account
    "Permanent Workforce Solutions LLC"

--- topics/classify/apply_topics.py
+++ topics/classify/apply_topics.py
@@ -14,8 +14,9 @@ def get_all_tweets(directory = None):
         df = pd.concat([df, user_df], ignore_index=True)
     return df
 
+def get_docs_topics():
+    get_all_tweets("nursetweets")['text'].to_csv("Dissertation/topics/all_nurse_tweets.csv")
+    df = model.get_document_info(get_all_tweets("nursetweets")['nouns'])
+    df["original_text"] = get_all_tweets("nursetweets")['text']
+    df.to_csv("Dissertation/topics/docs_topics.csv")
 
-get_all_tweets("nursetweets")['text'].to_csv("Dissertation/topics/all_nurse_tweets.csv")
-df = model.get_document_info(get_all_tweets("nursetweets")['nouns'])
-df["original_text"] = get_all_tweets("nursetweets")['text']
-df.to_csv("Dissertation/topics/docs_topics.csv")
