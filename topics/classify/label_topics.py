460652f9d0be9a6f202de05bd8ef12963d5bf2e6
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-02-08 12:22:41 +0000

    remove spam accounts

--- topics/classify/label_topics.py
+++ topics/classify/label_topics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("Dissertation/topics/docs_topics.csv")
+df = pd.read_csv("topics/docs_topics.csv")
 
 df = df[df["Representative_document"]==True]
 
-df.to_csv("representative_docs.csv")
+df.to_csv("topics/representative_docs.csv")
