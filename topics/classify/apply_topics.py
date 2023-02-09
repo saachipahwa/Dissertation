120678db89b213bc47239210ce18091672825174
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-02-09 12:47:13 +0000

    samples for nurse 10 topics

--- topics/classify/apply_topics.py
+++ topics/classify/apply_topics.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 from bertopic import BERTopic
 
-model = BERTopic.load("nursetweets_10_1_model")
 
 
 def get_all_tweets(directory=None):
@@ -17,19 +16,22 @@ def get_all_tweets(directory=None):
 
 
 def get_docs_topics():
+    model = BERTopic.load("nursetweets_10_1_model")
     # get_all_tweets("nursetweets")['text'].to_csv("Dissertation/topics/all_nurse_tweets.csv")
     df = model.get_document_info(get_all_tweets("nursetweets")['nouns'])
     df["original_text"] = get_all_tweets("nursetweets")['text']
     df.to_csv("Dissertation/topics/docs_topics.csv")
 
 
-get_docs_topics()
+# get_docs_topics()
 
 
 def get_sample_tweets():
-    df = pd.read_csv("Dissertation/topics/chosen_docs/docs_topics.csv")
+    df = pd.read_csv("topics/docs_topics.csv")
     for i in range(-1, 10):
         df_topic = df[df['Topic'] == i]
         df_topic = df_topic.sample(n=40, replace=False, random_state=1)
         df_topic.to_csv(
-            "Dissertation/topics/samples/topic_{}_sample.csv".format(i))
+            "topics/samples/topic_{}_sample.csv".format(i))
+
+get_sample_tweets()
