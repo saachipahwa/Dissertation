54e978c439737a4145d9495c22ac0d2f34cedd53
Saachi Pahwa <saachi.pahwa.19@ucl.ac.uk>
2023-02-09 12:46:54 +0000

    getting other models

--- topics/detection/topic_detection.py
+++ topics/detection/topic_detection.py
@@ -6,11 +6,13 @@ from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-directories =  ["nursetweets", "doctortweets", "teachertweets", "railtweets", "journalisttweets", "musiciantweets"]
+directories = ["nursetweets", "doctortweets", "teachertweets",
+               "railtweets", "journalisttweets", "musiciantweets"]
 directory_index = 0
 directory_name = "nursetweets"
 
-def get_all_tweets(directory = None):
+
+def get_all_tweets(directory=None):
     df = pd.DataFrame()
     for filename in os.listdir("Dissertation/"+directory):
         f = os.path.join("Dissertation/"+directory, filename)
@@ -19,17 +21,20 @@ def get_all_tweets(directory = None):
         df = pd.concat([df, user_df], ignore_index=True)
     return df
 
+
 def get_tweets():
     df = get_all_tweets(directories[directory_index])
     print("tweet count", len(df))
     return df['nouns'].astype(str).tolist()
 
+
 def get_embeddings(tweet_text):
     sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
     return sentence_model.encode(tweet_text, show_progress_bar=False)
 
-def get_topics_from(directory_name = "nursetweets", embeddings=None, nr_topics=None, ngram_max=1):
-    #set up model parameters
+
+def get_topics_from(directory_name="nursetweets", embeddings=None, nr_topics=None, ngram_max=1):
+    # set up model parameters
     topic_model = BERTopic(language="english",
                            calculate_probabilities=False,
                            verbose=True,
@@ -39,29 +44,32 @@ def get_topics_from(directory_name = "nursetweets", embeddings=None, nr_topics=N
                            )
     print("set up topic model. about to fit model")
 
-    #fit transform
+    # fit transform
     topics, probabilities = topic_model.fit_transform(tweet_text, embeddings)
     print("model has been fit")
 
-    #save model
+    # save model
     open("{}_{}_{}_model".format(directory_name, nr_topics, ngram_max), 'w+')
-    topic_model.save("{}_{}_{}_model".format(directory_name, nr_topics, ngram_max))
+    topic_model.save("{}_{}_{}_model".format(
+        directory_name, nr_topics, ngram_max))
     print("model has been saved", nr_topics, ngram_max)
 
-    #get details of topics
+    # get details of topics
     freq = topic_model.get_topic_info()
     print(freq)
 
-    #save details to csv
+    # save details to csv
     details_list = []
     for i in freq['Topic']:
-        details_list.append([x for x,y in topic_model.get_topic(i)])
+        details_list.append([x for x, y in topic_model.get_topic(i)])
 
     freq['details'] = details_list
-    freq.to_csv('Dissertation/topics/{}_topics_{}_{}.csv'.format(directory_name, nr_topics, ngram_max))
+    freq.to_csv(
+        'Dissertation/topics/{}_topics_{}_{}.csv'.format(directory_name, nr_topics, ngram_max))
     print("info saved to csv")
 
     return topic_model
+
 
 def pad_out_dict(dict):
     maxlength = 0
@@ -75,64 +83,76 @@ def pad_out_dict(dict):
 
     return dict
 
-#get tweet text
+
+# get tweet text
 tweet_text = get_tweets()
 print("got df")
 
-#pre-compute embeddings
+# pre-compute embeddings
 embeddings = get_embeddings(tweet_text)
 print("computing embeddings")
 
-#get topics
-# print("getting topics ", "5", "ngram", "1")
-# model_5_1 = get_topics_from(directory_name=directory_name, embeddings=embeddings, nr_topics=5, ngram_max=1)
-# print("getting topics ", "5", "ngram", "2")
-# model_5_2 = get_topics_from(directory_name=directory_name, nr_topics=5, embeddings=embeddings, ngram_max=2)
-# print("getting topics ", "5", "ngram", "3")
-# model_5_3 = get_topics_from(directory_name=directory_name, nr_topics=5, embeddings=embeddings, ngram_max=3)
-#
-print("getting topics ", "10", "ngram", "1")
-model_10_1 = get_topics_from(directory_name=directory_name,  embeddings=embeddings, nr_topics=10, ngram_max=1)
-# print("getting topics ", "10", "ngram", "2")
-# model_10_2 = get_topics_from(directory_name=directory_name, nr_topics=10, embeddings=embeddings, ngram_max=2)
-# print("getting topics ", "10", "ngram", "3")
-# model_10_3 = get_topics_from(directory_name=directory_name, nr_topics=10, embeddings=embeddings, ngram_max=3)
-#
-# print("getting topics ", "15", "ngram", "1")
-# model_15_1 = get_topics_from(directory_name=directory_name, embeddings=embeddings, nr_topics=15, ngram_max=1)
-# print("getting topics ", "15", "ngram", "2")
-# model_15_2 = get_topics_from(directory_name=directory_name, nr_topics=15, embeddings=embeddings, ngram_max=2)
-# print("getting topics ", "15", "ngram", "3")
-# model_15_3 = get_topics_from(directory_name=directory_name, nr_topics=15, embeddings=embeddings, ngram_max=3)
-#
-# print("getting topics ", "20", "ngram", "1")
-# model_20_1 = get_topics_from(directory_name="nursetweets",  embeddings=embeddings, nr_topics=20, ngram_max=1)
-# print("getting topics ", "20", "ngram", "2")
-# model_20_2 = get_topics_from(directory_name="nursetweets", nr_topics=20, embeddings=embeddings, ngram_max=2)
-# print("getting topics ", "20", "ngram", "3")
-# model_20_3 = get_topics_from(directory_name="nursetweets", nr_topics=20, embeddings=embeddings, ngram_max=3)
-#
+# get topics
+print("getting topics ", "5", "ngram", "1")
+model_5_1 = get_topics_from(
+    directory_name=directory_name, embeddings=embeddings, nr_topics=5, ngram_max=1)
+print("getting topics ", "5", "ngram", "2")
+model_5_2 = get_topics_from(
+    directory_name=directory_name, nr_topics=5, embeddings=embeddings, ngram_max=2)
+print("getting topics ", "5", "ngram", "3")
+model_5_3 = get_topics_from(
+    directory_name=directory_name, nr_topics=5, embeddings=embeddings, ngram_max=3)
+
+# print("getting topics ", "10", "ngram", "1")
+# model_10_1 = get_topics_from(directory_name=directory_name,  embeddings=embeddings, nr_topics=10, ngram_max=1)
+print("getting topics ", "10", "ngram", "2")
+model_10_2 = get_topics_from(
+    directory_name=directory_name, nr_topics=10, embeddings=embeddings, ngram_max=2)
+print("getting topics ", "10", "ngram", "3")
+model_10_3 = get_topics_from(
+    directory_name=directory_name, nr_topics=10, embeddings=embeddings, ngram_max=3)
+
+print("getting topics ", "15", "ngram", "1")
+model_15_1 = get_topics_from(
+    directory_name=directory_name, embeddings=embeddings, nr_topics=15, ngram_max=1)
+print("getting topics ", "15", "ngram", "2")
+model_15_2 = get_topics_from(
+    directory_name=directory_name, nr_topics=15, embeddings=embeddings, ngram_max=2)
+print("getting topics ", "15", "ngram", "3")
+model_15_3 = get_topics_from(
+    directory_name=directory_name, nr_topics=15, embeddings=embeddings, ngram_max=3)
+
+print("getting topics ", "20", "ngram", "1")
+model_20_1 = get_topics_from(
+    directory_name="nursetweets",  embeddings=embeddings, nr_topics=20, ngram_max=1)
+print("getting topics ", "20", "ngram", "2")
+model_20_2 = get_topics_from(
+    directory_name="nursetweets", nr_topics=20, embeddings=embeddings, ngram_max=2)
+print("getting topics ", "20", "ngram", "3")
+model_20_3 = get_topics_from(
+    directory_name="nursetweets", nr_topics=20, embeddings=embeddings, ngram_max=3)
+
 # model5_1_docs = pd.DataFrame.from_dict(pad_out_dict(model_5_1.get_representative_docs())).sort_index(axis=1)
 # model5_1_docs.to_csv('Dissertation/topics/nursetweets_5_1_docs.csv')
 # model5_2_docs = pd.DataFrame.from_dict(pad_out_dict(model_5_2.get_representative_docs())).sort_index(axis=1)
 # model5_2_docs.to_csv('Dissertation/topics/nursetweets_5_2_docs.csv')
 # model5_3_docs = pd.DataFrame.from_dict(pad_out_dict(model_5_3.get_representative_docs())).sort_index(axis=1)
 # model5_3_docs.to_csv('Dissertation/topics/nursetweets_5_3_docs.csv')
-#
+
 # model10_1_docs = pd.DataFrame.from_dict(pad_out_dict(model_10_1.get_representative_docs())).sort_index(axis=1)
 # model10_1_docs.to_csv('Dissertation/topics/nursetweets_10_1_docs.csv')
 # model10_2_docs = pd.DataFrame.from_dict(pad_out_dict(model_10_2.get_representative_docs())).sort_index(axis=1)
 # model10_2_docs.to_csv('Dissertation/topics/nursetweets_10_2_docs.csv')
 # model10_3_docs = pd.DataFrame.from_dict(pad_out_dict(model_10_3.get_representative_docs())).sort_index(axis=1)
 # model10_3_docs.to_csv('Dissertation/topics/nursetweets_10_3_docs.csv')
-#
+
 # model15_1_docs = pd.DataFrame.from_dict(pad_out_dict(model_15_1.get_representative_docs())).sort_index(axis=1)
 # model15_1_docs.to_csv('Dissertation/topics/nursetweets_15_1_docs.csv')
 # model15_2_docs = pd.DataFrame.from_dict(pad_out_dict(model_15_2.get_representative_docs())).sort_index(axis=1)
 # model15_2_docs.to_csv('Dissertation/topics/nursetweets_15_2_docs.csv')
 # model15_3_docs = pd.DataFrame.from_dict(pad_out_dict(model_15_3.get_representative_docs())).sort_index(axis=1)
 # model15_3_docs.to_csv('Dissertation/topics/nursetweets_15_3_docs.csv')
-#
+
 # model20_1_docs = pd.DataFrame.from_dict(pad_out_dict(model_20_1.get_representative_docs())).sort_index(axis=1)
 # model20_1_docs.to_csv('Dissertation/topics/nursetweets_20_1_docs.csv')
 # model20_2_docs = pd.DataFrame.from_dict(pad_out_dict(model_20_2.get_representative_docs())).sort_index(axis=1)
