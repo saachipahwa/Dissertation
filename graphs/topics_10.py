76430dac0c332a97c91963726429758000d3577f
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-02-09 13:04:31 +0000

    topics with dates doc

--- graphs/topics_10.py
+++ graphs/topics_10.py
@@ -1,8 +1,7 @@
-import os
-
 import pandas as pd
 from bertopic import BERTopic
 
+df = pd.DataFrame(columns = ['original_tweet', 'date', ''])
 
 
 def get_all_tweets(directory=None):
@@ -14,23 +13,12 @@ def get_all_tweets(directory=None):
         df = pd.concat([df, user_df], ignore_index=True)
     return df
 
-
-def get_docs_topics():
+def get_topics_with_dates():
     model = BERTopic.load("nursetweets_10_1_model")
     df = model.get_document_info(get_all_tweets("nursetweets")['nouns'])
     df["original_text"] = get_all_tweets("nursetweets")['text']
-    df.to_csv("Dissertation/topics/docs_topics.csv")
+    df["created_at"] = get_all_tweets("nursetweets")['created_at']
 
+    df.to_csv("Dissertation/topics/topics_with_dates.csv")
 
-# get_docs_topics()
-
-
-def get_sample_tweets():
-    df = pd.read_csv("topics/docs_topics.csv")
-    for i in range(-1, 10):
-        df_topic = df[df['Topic'] == i]
-        df_topic = df_topic.sample(n=40, replace=False, random_state=1)
-        df_topic.to_csv(
-            "topics/samples/topic_{}_sample.csv".format(i))
-
-get_sample_tweets()
+get_topics_with_dates()
