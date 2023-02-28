794e08e26c2b3cc46f1bff9ed5b21c9e529ccdb9
Saachi Pahwa <saachi.pahwa.19@ucl.ac.uk>
2023-02-28 10:55:48 +0000

    added topic label

--- graphs/topics_10.py
+++ graphs/topics_10.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from bertopic import BERTopic
 import os
+import numpy as np
 
 
 def get_all_tweets(directory=None):
@@ -28,26 +29,44 @@ def dynamic_topic_modelling():
     dates_df = pd.read_csv("Dissertation/graphs/topics_with_dates.csv")
     model_copy = BERTopic.load("nursetweets_10_1_model_copy")
     model_copy.merge_topics(get_all_tweets("nursetweets")['nouns'],
-                            [[-1, 2], [0, 1, 3, 4, 5, 6, 7, 8, 9]])
+                            [[2], [0, 1, 3, 4, 5, 6, 7, 8, 9]])
     model_copy.set_topic_labels({-1: "Work", 0: "Life"})
     print(model_copy.get_topic_info())
     topics_over_time = model_copy.topics_over_time(
         dates_df['Document'], dates_df['created_at'], datetime_format="%Y-%m-%d %H:%M:%S+00:00", nr_bins=30)
     fig = model_copy.visualize_topics_over_time(
-        topics_over_time, topics=[-1, 0], custom_labels=True)
+        topics_over_time, topics=[0, 1], custom_labels=True)
     fig.show()
 
 
 # dynamic_topic_modelling()
 
-def words_per_topic():
-    model_copy = BERTopic.load("nursetweets_10_1_model_copy")
-    model_copy.merge_topics(get_all_tweets("nursetweets")['nouns'],
-                            [[-1, 2], [0, 1, 3, 4, 5, 6, 7, 8, 9]])
-    model_copy.set_topic_labels({-1: "Work", 0: "Life"})
 
-    fig = model_copy.visualize_barchart(topics=[-1, 0], custom_labels=True)
-    fig.show()
+def add_topic_label():
+    df = pd.read_csv("Dissertation/graphs/topics_with_dates.csv")
+
+    conditions = [
+        (df['Topic'] == "-1"),
+        (df['Topic'] == "2"),
+        (df['Topic'] != "-1") & (df['Topic'] != "2")
+    ]
+
+    values = ["None", "Work", "Life"]
+
+    df['tier'] = np.select(conditions, values)
+    df.to_csv("Dissertation/graphs/topics_with_dates.csv")
 
 
-words_per_topic()
+add_topic_label()
+
+
+# def words_per_topic():
+#     model_copy = BERTopic.load("nursetweets_10_1_model_copy")
+#     model_copy.merge_topics(get_all_tweets("nursetweets")['nouns'],
+#                             [[-1, 2], [0, 1, 3, 4, 5, 6, 7, 8, 9]])
+#     model_copy.set_topic_labels({-1: "Work", 0: "Life"})
+
+#     fig = model_copy.visualize_barchart(topics=[-1, 0], custom_labels=True)
+#     fig.show()
+
+# words_per_topic()
