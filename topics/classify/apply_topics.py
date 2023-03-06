ab79ebe10c066308bab0b685d532dc167859125e
Saachi Pahwa <saachi.pahwa.19@ucl.ac.uk>
2023-03-06 13:25:59 +0000

    get docs topics and rep tweets

--- topics/classify/apply_topics.py
+++ topics/classify/apply_topics.py
@@ -1,9 +1,6 @@
 import os
-
 import pandas as pd
 from bertopic import BERTopic
-
-
 
 def get_all_tweets(directory=None):
     df = pd.DataFrame()
@@ -14,25 +11,23 @@ def get_all_tweets(directory=None):
         df = pd.concat([df, user_df], ignore_index=True)
     return df
 
-
 def get_docs_topics():
-    model = BERTopic.load("nursetweets_10_1_model")
+    model = BERTopic.load("1000_models/nursetweets_10_1_model")
     df = model.get_document_info(get_all_tweets("nursetweets")['nouns'])
     df["original_text"] = get_all_tweets("nursetweets")['text']
-    df.to_csv("Dissertation/topics/docs_topics.csv")
+    df.to_csv("Dissertation/topics/docs_topics_1000.csv")
 
-
-# get_docs_topics()
+get_docs_topics()
 
 def get_representative_tweets():
-    df = pd.read_csv("Dissertation/topics/docs_topics.csv")
+    df = pd.read_csv("Dissertation/topics/docs_topics_1000.csv")
     df = df[df['Representative_document']==True]
-    df.to_csv("Dissertation/topics/rep_docs.csv")
+    df.to_csv("Dissertation/topics/rep_docs_1000.csv")
 
 get_representative_tweets()
 
 def get_sample_tweets():
-    df = pd.read_csv("Dissertation/topics/docs_topics.csv")
+    df = pd.read_csv("Dissertation/topics/docs_topics_1000.csv")
     for i in range(-1, 10):
         df_topic = df[df['Topic'] == i]
         df_topic = df_topic.sample(n=40, replace=False, random_state=1)
--- topics/classify/label_topics.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("topics/docs_topics.csv")
-
-df = df[df["Representative_document"]==True]
-
-df.to_csv("topics/representative_docs.csv")
