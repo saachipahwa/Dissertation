b0551789f6836408e2b8893474b07a33939cdc54
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-03-29 15:13:33 +0100

    clean up teacher follower file

---
+++ data_functions/clean_follower_file.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+
+directory = "teachertweets"
+followers_df_name = "data/filteredNEUfollowers.csv"
+followers_df = pd.read_csv(followers_df_name)
+count_removed = 0
+
+for index, row in followers_df.iterrows():
+    f = "{}/{}.csv".format(directory, str(int(row["id"])))
+
+    if not os.path.isfile(f):
+        followers_df.drop(index=index, inplace=True)
+        count_removed+=1
+
+followers_df.to_csv(followers_df_name)
+print(count_removed, "removed")
+
--- data_functions/remove_empty.py
+++ data_functions/remove_empty.py
@@ -3,19 +3,21 @@
 import os
 import pandas as pd
 
-directory = "railtweets"
+directory = "teachertweets"
 
-followers_df_name = "data/filteredRMTfollowers.csv"
+followers_df_name = "data/filteredNEUfollowers.csv"
 
 followers_df = pd.read_csv(followers_df_name)
-count_removed=0
+count_removed = 0
 for index, row in followers_df.iterrows():
     f = "{}/{}.csv".format(directory, str(int(row["id"])))
     if os.path.isfile(f):
         df = pd.read_csv(f)
+        print(len(df))
         if len(df) == 0:
             count_removed+=1
             os.remove(f)
+            followers_df.drop(index=index, inplace=True)
 
 followers_df.to_csv(followers_df_name)
 print(count_removed, "removed")
