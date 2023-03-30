bb7a70aaff87f192576378951182dfdd37768c16
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-03-30 11:14:24 +0100

    removed lmao and lmfao from tweets

--- data_functions/filter_tweets.py
+++ data_functions/filter_tweets.py
@@ -97,7 +97,7 @@ def get_nouns(text):
     text = text.split(" ")
     tags = nltk.pos_tag(text)
     new_text = ""
-    haha_lol_omg = re.compile(r"^(ha)+$|^haha|lol(ol)*|omg(omg)*|wow(wow)*")
+    haha_lol_omg = re.compile(r"^(ha)+$|^haha|lol(ol)*|omg(omg)*|wow(wow)*|lmao(o)*|lmfao(o)*")
     contraction_regex = re.compile(r"^[A-Za-z]+['|’][A-Za-z]+$")
 
     for a, b in tags:
